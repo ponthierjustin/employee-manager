605c7e214b31aecb4588b4da943913920b7c8165
Justin <ponthierjustin@gmail.com>
2020-07-09 17:00:31 -0400

    basic server established to mysql

---
+++ management.js
@@ -0,0 +1,18 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  port: 3306,
+
+  user: "root",
+
+  password: "TheLaughTrack123!!",
+  database: "managementDB",
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+});
